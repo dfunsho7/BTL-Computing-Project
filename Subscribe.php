ff07f391182dbe2b3cf6c7c9d724fdc187217780
Funsho <danielogunseye@hotmail.co.uk>
2020-09-15 08:46:15 +0100

    Create Subscribe.php

---
+++ Subscribe.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+        html, body {
+            height: 100%;
+          }
+          
+          .full-height {
+            height: 100%;
+          }
+          
+    </style>
+  </head>
+  <body>
+
+  <div class="container-fluid bg-danger text-white full-height my-auto">
+      <div class="container h-100">
+          <h3 class="text-center pt-4">YOU CAN SUBSCRIBE ANY RECREATIONAL ACTIVITY</h3>
+          <div class="row h-100 justify-content-center align-items-center">
+              <div class="col-sm-4"></div>
+              <div class="col-sm-4 text-center">
+                <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="NCPTHCGJR6PL8">
+                    <table class="mb-2">
+                    <tr><td><input type="hidden" name="on0" value="PAY FOR">PAY FOR</td></tr><tr><td><select name="os0">
+                        <option value="FITNESS LAB">FITNESS LAB : £100.00 GBP - monthly</option>
+                        <option value="SWIMMING WORLD">SWIMMING WORLD : £100.00 GBP - monthly</option>
+                        <option value="PROFORMANCE SPORTS">PROFORMANCE SPORTS : £150.00 GBP - monthly</option>
+                        <option value="HIGH OCTANE">HIGH OCTANE : £200.00 GBP - monthly</option>
+                        <option value="Counselling Services">Counselling Services : £50.00 GBP - monthly</option>
+						<option value="Onside Youth Zone">Onside Youth Zone : £5.00 GBP - yearly</option>
+                    </select> </td></tr>
+                    </table>
+                    <input type="hidden" name="currency_code" value="GBP">
+                    <input type="image" src="https://www.sandbox.paypal.com/en_GB/i/btn/btn_subscribe_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
+                    <img alt="" border="0" src="https://www.sandbox.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+                </form>                                     
+              </div>
+              <div class="col-sm-4"></div>
+          </div>
+      </div>
+  </div>
+
+  </body>
+</html>
