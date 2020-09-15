97e241ad42a5441d159fdb8b6547226ce2436559
Funsho <danielogunseye@hotmail.co.uk>
2020-09-15 08:49:32 +0100

    Create Recreation.php

---
+++ Recreation.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  </head>
+  <body>
+  <div class="container-fluid">
+          <h4 class="text-center text-danger mt-4 mb-2">Need To Do Something For Recreation</h4>
+          <div class="row mt-5 mb-5">
+              <div class="col-sm-6 pr-2">
+                <div class="card mb-3">
+                    <div class="row no-gutters">
+                      <div class="col-md-5">
+                        <img src="img/gym.jpg" class="card-img" alt="..." height="100%">
+                      </div>
+                      <div class="col-md-7 pl-4">
+                          <h5 class="card-title">FITNESS LAB</h5>
+                          <p class="card-text"><small>Welcome to Fitness Lab.  Its A Fitness Based Gym Center, Sport, Functional Training, Boxing, Spinning, Guided Classes and Personal Training. Subscribe Now</small></p>
+                          <a href="subscribe.php" class="btn btn-primary" target="blank">Paypal</a>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-sm-6 pr-2">
+                <div class="card mb-3">
+                    <div class="row no-gutters">
+                      <div class="col-md-5">
+                        <img src="img/swimming.jpg" class="card-img" alt="..." height="100%">
+                      </div>
+                      <div class="col-md-7 pl-4">
+                          <h5 class="card-title">SWIMMING WORLD</h5>
+                          <p class="card-text"><small>Welcome to Swimming World.  Its A Swimming Training Center For Sports, Guided Classes and Personal Training By Experts Swimming Trainers. Subscribe Now</small></p>
+                          <a href="subscribe.php" class="btn btn-primary" target="blank">Paypal</a>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-sm-6 pr-2 mt-5">
+                <div class="card mb-3">
+                    <div class="row no-gutters">
+                      <div class="col-md-5">
+                        <img src="img/sports.jpg" class="card-img" alt="..." height="100%">
+                      </div>
+                      <div class="col-md-7 pl-4">
+                          <h5 class="card-title">PROFORMANCE SPORTS</h5>
+                          <p class="card-text"><small>Welcome to Proformance Sports Training uses cutting edge sports science to get you to the next level.  It Trains In All Sports. Subscribe Now</small></p>
+                          <a href="subscribe.php" class="btn btn-primary" target="blank">Paypal</a>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-sm-6 pr-2 mt-5">
+                <div class="card mb-3">
+                    <div class="row no-gutters">
+                      <div class="col-md-5">
+                        <img src="img/octane.jpg" class="card-img" alt="..." height="100%">
+                      </div>
+                      <div class="col-md-7 pl-4">
+                          <h5 class="card-title">HIGH OCTANE</h5>
+                          <p class="card-text"><small>Welcome to High Octane.  High-Octane Adventure Sports In UK That Will Get Your Heart In Your Throat! Bungy Jumping, Rock Climbing Etc Available. Subscribe Now</small></p>
+                          <a href="subscribe.php" class="btn btn-primary" target="blank">Paypal</a>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+
+            </div>
+  </div>
+
+  </body>
+</html>
